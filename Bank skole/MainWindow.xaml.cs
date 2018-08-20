8f9d4c53dc4bfd34862307552c6c4a754fa50e9e
TunaWithSalmon <rasmus@davidsen.biz>
2018-08-20 10:36:21 +0200

    Initial commit

---
+++ Bank skole/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Bank_skole
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
