9426e2f6a8f24abd5c9a7c49815c00627eb7db96
JoKrH <31029379+JoKrH@users.noreply.github.com>
2018-08-21 11:44:02 +0200

    Opdate 21-08-2018

--- Bank skole/MainWindow.xaml.cs
+++ Bank skole/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace Bank_skole
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bank_skole
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -9,5 +12,19 @@
         {
             InitializeComponent(); 
         }
+        public class MyViewModel
+        {
+            public List<String> Items
+            {
+                get { return new List<String> { "One", "Two", "Three" }; }
+            }
+        }
+
+        //This can be done in the Loaded event of the page:
+        DataContext = private MyViewModel();
+    }
+
+    internal class DataContext
+    {
     }
 }
