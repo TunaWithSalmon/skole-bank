8702d7a7c86d68dbc2b3c19f99a8ff74bf17d3de
JoKrH <31029379+JoKrH@users.noreply.github.com>
2018-08-22 14:05:23 +0200

    Transaction

---
+++ Banker/Db/TransactionDB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Banker.Db;
+using Banker.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace bacnk_web.Models
+{
+    public class TransactionDB : DB<Transaction>
+    {
+        public TransactionDB(IMongoDatabase db) : base("Transaction", db)
+        {
+        }
+
+        public async Task CreateTransaction(
+            string name,
+            int amount
+        )
+        {
+            var newTrans = new Transaction()
+            {
+                TransactionName = name,
+                Amount = amount,
+            };
+            await Insert(newTrans);
+        }
+
+        public async Task<Transaction> GetTransaction(string id)
+        {
+            return await GetOne(new BsonDocument("_id", ObjectId.Parse(id)));
+        }
+
+        public async Task<List<Transaction>> GetCustomers()
+        {
+            return await GetAll(new BsonDocument());
+        }
+
+    }
+}
