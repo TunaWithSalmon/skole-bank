458c52e21203619c5031d90bd60c460f23b23f16
TunaWithSalmon <rasmus@davidsen.biz>
2018-08-22 09:03:15 +0200

    working now

---
+++ Banker/Controllers/CustomerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Banker.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        public string Post([FromBody]string value)
+        {
+            return $"{value}";
+        }
+
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        public void Delete(int id)
+        {
+        }
+    }
+}
